62416dce89d8a5d97a73d162928817460a6ae26a
王浩 <hao.wang@qingclass.com>
2020-06-29 13:50:29 +0800

    5

--- 5. 最长回文子串.py
+++ 5. 最长回文子串.py
@@ -1,4 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
-        return ''
+        n = len(s)
+        d = [[False] * n for _ in range(n)]
+        sub = ''
+        for l in range(n):
+            for i in range(n):
+                j = i + l
+                if j >= n:
+                    break
+                if i == j:
+                    d[i][j] = True
+                elif j == i + 1:
+                    d[i][j] = s[i] == s[j]
+                else:
+                    d[i][j] = d[i+1][j-1] and s[i] == s[j]
+                if d[i][j] and l + 1 > len(sub):
+                    sub = s[i:j+1]
+        return sub
+
+result = Solution().longestPalindrome('cbbd')
+print(result)
